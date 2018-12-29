52535317cfc1b02e3a642c940a16dde94aa35c41
Douglas Gonçalves <gondouglas43@gmail.com>
2018-12-29 00:51:40 -0200

    create architeture folders

---
+++ UnityTest/Assets/Scripts/0-Infraestructure/0-Models/0-Aggregate/BaseCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base for every characters
+/// </summary>
+public abstract class BaseCharacter : MonoBehaviour {
+
+    public BasicAttribute basicAttribute;
+
+}
---
+++ UnityTest/Assets/Scripts/0-Infraestructure/0-Models/2-ValueObjects/BasicAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/// <summary>
+/// Basic attributes for a character
+/// </summary
+[Serializable]
+public class BasicAttribute {
+
+    public int maxHP;
+    public int maxSP;
+    public int maxMP;
+
+    private float CurrentHP { get; set; }
+    private float CurrentSP { get; set; }
+    private float CurrentMP { get; set; }
+}
